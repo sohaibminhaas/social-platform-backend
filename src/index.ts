512345fb3b7a9084aeb5d7aed6dac2a9946d7a66
Sohaib Riaz <sohaib@paynest.com>
2022-12-30 11:07:01 +0500

    cors added

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,17 @@ import webRoutes from "./routes/web-routes";
 import bodyparser from "body-parser";
 import dotenv from 'dotenv';
 dotenv.config();
+import cors from "cors";
 
+app.use(cors());
 app.use(
     bodyparser.urlencoded({
         extended: true,
     })
 );
-
 app.use(bodyparser.json());
-
 app.use('/admin', adminRoutes())
 app.use('/', webRoutes())
-
 const port: string | number  = process.env.PORT ?? 5000;
 app.listen(port, () => {
     console.log(`server is listing on port: ${port}`);
